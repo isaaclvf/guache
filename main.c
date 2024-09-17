4f1fd75a0527355ad2a0a26f19fa9c1474d857c8
Isaac Farias <isaacfarias006@gmail.com>
2024-09-17 16:50:04 -0300

    add drawing points

--- main.c
+++ main.c
@@ -7,33 +7,90 @@
 int windowWidth = 800;
 int windowHeight = 600;
 
+typedef struct {
+  float x, y;
+  float color[3]; // RGB
+  float size;
+} Point;
+
+typedef struct PointNode {
+  Point point;
+  struct PointNode *next;
+} PointNode;
+
+typedef struct PointList {
+  PointNode *head;
+} PointList;
+
+GLfloat maxPointSize;
+
+PointList pointList;
+
+void addPoint(float x, float y, float red, float green, float blue,
+              float size) {
+  PointNode *newNode = (PointNode *)malloc(sizeof(PointNode));
+
+  newNode->point.x = x;
+  newNode->point.y = y;
+  newNode->point.color[0] = red;
+  newNode->point.color[1] = green;
+  newNode->point.color[2] = blue;
+  newNode->point.size = size;
+
+  newNode->next = pointList.head;
+  pointList.head = newNode;
+}
+
+void renderAllPoints() {
+  PointNode *current = pointList.head;
+
+  // TODO: Encontrar uma forma de renderizar botões
+  // de tamanhos diferentes. Pelo visto, não dá pra usar
+  // glPointSize(current->point.size) dentro do glBegin.
+  glPointSize(5.0f);
+
+  glBegin(GL_POINTS);
+  while (current != NULL) {
+    glColor3f(current->point.color[0], current->point.color[1],
+              current->point.color[2]);
+
+    glVertex2f(current->point.x, current->point.y);
+    current = current->next;
+  }
+  glEnd();
+}
+
+void freePointList() {
+  PointNode *current = pointList.head;
+  while (current != NULL) {
+    PointNode *next = current->next;
+    free(current);
+    current = next;
+  }
+}
+
+void initLists() { pointList.head = NULL; }
+
 void init() {
-  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+  initLists();
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
   glMatrixMode(GL_PROJECTION);
   gluOrtho2D(0.0f, windowWidth, 0.0f, windowHeight);
 
-  // Para o antialiasing
-  glEnable(GL_MULTISAMPLE);
-  glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_LINE_SMOOTH);
+  // Query the maximum point size allowed by the system
+  glGetFloatv(GL_POINT_SIZE_MAX, &maxPointSize);
 }
 
 void display() {
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(1.0f, 0.0f, 0.0f);
-
-  glBegin(GL_POLYGON);
-  glVertex2f((float)windowWidth / 2, 2 * (float)windowHeight / 3);
-  glVertex2f((float)windowWidth / 3, (float)windowHeight / 3);
-  glVertex2f(2 * (float)windowWidth / 3, (float)windowHeight / 3);
-  glEnd();
-
+  renderAllPoints();
   glFlush();
 }
 
+// Demo de como usar antialiasing
 void displayStickman() {
   // Ativar antialiasing para linhas
   glEnable(GL_LINE_SMOOTH);
@@ -76,15 +133,35 @@ void displayStickman() {
   glDisable(GL_LINE_SMOOTH);
 }
 
+// Mouse callback function to handle mouse clicks
+void onMouseClick(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+    // Convert window coordinates to OpenGL coordinates
+    float worldX = (float)x;
+    float worldY = (float)(windowHeight - y); // Flip Y-coordinate
+    addPoint(worldX, worldY, 0.0f, 0.0f, 0.0f, 15.0f);
+    // Redesenhar a janela
+    glutPostRedisplay();
+  }
+}
+
 int main(int argc, char *argv[]) {
   glutInit(&argc, argv);
+  // Mudar para GLUT_DOUBLE depois
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_MULTISAMPLE);
   glutInitWindowSize(windowWidth, windowHeight);
   glutInitWindowPosition(100, 100);
   glutCreateWindow("Guache - 2D Painter");
+  // TODO: Ver glutReshapeFunc();
+
+  // Opções para antialiasing
+  // glEnable(GL_MULTISAMPLE);
+  // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  // glEnable(GL_LINE_SMOOTH);
 
   init();
-  glutDisplayFunc(displayStickman);
+  glutDisplayFunc(display);
+  glutMouseFunc(onMouseClick);
   glutMainLoop();
 
   return EXIT_SUCCESS;
