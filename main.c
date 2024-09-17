a9a593d87dedc231fe1b3de2f3bfe71d7938ce91
Isaac Farias <isaacfarias006@gmail.com>
2024-09-17 19:26:13 -0300

    add data structures for lines and polygons

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_VERTICES 10
 
 int windowWidth = 800;
 int windowHeight = 600;
@@ -13,16 +16,49 @@ typedef struct {
   float size;
 } Point;
 
+typedef struct {
+  float x0, y0;   // Start
+  float x1, y1;   // End
+  float color[3]; // RGB
+  // float thickness;
+} Line;
+
+typedef struct {
+  int vertexCount;
+  float vertices[MAX_VERTICES][2]; // Coordenadas dos vertices
+  float color[3];                  // RGB
+} Polygon;
+
 typedef struct PointNode {
   Point point;
   struct PointNode *next;
 } PointNode;
 
+typedef struct LineNode {
+  Line line;
+  struct LineNode *next;
+} LineNode;
+
+typedef struct PolygonNode {
+  Polygon polygon;
+  struct PolygonNode *next;
+} PolygonNode;
+
 typedef struct PointList {
   PointNode *head;
 } PointList;
 
-PointList pointList;
+typedef struct LineList {
+  LineNode *head;
+} LineList;
+
+typedef struct PolygonList {
+  PolygonNode *head;
+} PolygonList;
+
+PointList pointList = {NULL};
+LineList lineList = {NULL};
+PolygonList polygonList = {NULL};
 
 void addPoint(float x, float y, float red, float green, float blue,
               float size) {
@@ -37,6 +73,45 @@ void addPoint(float x, float y, float red, float green, float blue,
 
   newNode->next = pointList.head;
   pointList.head = newNode;
+}
+
+void addLine(float x0, float y0, float x1, float y1, float red, float green,
+             float blue) {
+  LineNode *newNode = (LineNode *)malloc(sizeof(LineNode));
+
+  newNode->line.x0 = x0;
+  newNode->line.y0 = y0;
+  newNode->line.x1 = x1;
+  newNode->line.y1 = y1;
+  newNode->line.color[0] = red;
+  newNode->line.color[1] = green;
+  newNode->line.color[2] = blue;
+
+  newNode->next = lineList.head;
+  lineList.head = newNode;
+}
+
+void addPolygon(float vertices[][2], int vertexCount, float red, float green,
+                float blue) {
+  if (vertexCount < 3) {
+    printf("Erro: Um polígono precisa ter pelo menos 3 vertices.\n");
+    return;
+  }
+
+  PolygonNode *newNode = (PolygonNode *)malloc(sizeof(PolygonNode));
+
+  newNode->polygon.vertexCount = vertexCount;
+  for (int i = 0; i < vertexCount; i++) {
+    newNode->polygon.vertices[i][0] = vertices[i][0];
+    newNode->polygon.vertices[i][1] = vertices[i][1];
+  }
+
+  newNode->polygon.color[0] = red;
+  newNode->polygon.color[1] = green;
+  newNode->polygon.color[2] = blue;
+
+  newNode->next = polygonList.head;
+  polygonList.head = newNode;
 }
 
 void renderAllPoints() {
@@ -58,6 +133,38 @@ void renderAllPoints() {
   glEnd();
 }
 
+void renderAllLines() {
+  LineNode *current = lineList.head;
+
+  glBegin(GL_LINES);
+  while (current != NULL) {
+    glColor3f(current->line.color[0], current->line.color[1], current->line.color[2]);
+
+    glVertex2f(current->line.x0, current->line.y0);
+    glVertex2f(current->line.x1, current->line.y1);
+
+    current = current->next;
+  }
+  glEnd();
+}
+
+void renderAllPolygons() {
+  PolygonNode *current = polygonList.head;
+
+  while (current != NULL) {
+    glColor3f(current->polygon.color[0], current->polygon.color[1], current->polygon.color[2]);
+
+    glBegin(GL_POLYGON);
+
+    for (int i = 0; i < current->polygon.vertexCount; i++) {
+      glVertex2f(current->polygon.vertices[i][0], current->polygon.vertices[i][1]);
+    }
+
+    glEnd();
+    current = current->next;
+  }
+}
+
 void freePointList() {
   PointNode *current = pointList.head;
   while (current != NULL) {
@@ -67,10 +174,25 @@ void freePointList() {
   }
 }
 
-void initLists() { pointList.head = NULL; }
+void freeLineList() {
+  LineNode *current = lineList.head;
+  while (current != NULL) {
+    LineNode *next = current->next;
+    free(current);
+    current = next;
+  }
+}
+
+void freePolygonList() {
+  PolygonNode *current = polygonList.head;
+  while (current != NULL) {
+    PolygonNode *next = current->next;
+    free(current);
+    current = next;
+  }
+}
 
 void init() {
-  initLists();
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
   glMatrixMode(GL_PROJECTION);
@@ -82,7 +204,11 @@ void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
   renderAllPoints();
+  renderAllLines();
+  renderAllPolygons();
+
   glFlush();
+  // glutSwapBuffers();
 }
 
 // Demo de como usar antialiasing
@@ -128,10 +254,9 @@ void displayStickman() {
   glDisable(GL_LINE_SMOOTH);
 }
 
-// Mouse callback function to handle mouse clicks
 void onMouseClick(int button, int state, int x, int y) {
   if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-    // Convert window coordinates to OpenGL coordinates
+    // Converte coordenadas da janela para coordenadas do OpenGL
     float worldX = (float)x;
     float worldY = (float)(windowHeight - y); // Flip Y-coordinate
     addPoint(worldX, worldY, 0.0f, 0.0f, 0.0f, 15.0f);
